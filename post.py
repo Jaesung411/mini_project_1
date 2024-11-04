a3855a503cfb1499f6bb5ee1a19a57375fddcc1c
myeongsoyi <myeongsoyi@gmail.com>
2024-11-04 20:09:13 +0900

    feat: review inset, update, delete return 수정

--- post.py
+++ post.py
@@ -1,5 +1,8 @@
 from flask import *
 from postdb import *
+from DB.storedb import *
+from DB.menudb import *
+from DB.imagedb import *
 
 post_bp = Blueprint('post', __name__)
 post_bp.secret_key = '1234'
@@ -13,17 +16,26 @@ def review_list_detail(store_id):
 def add_review(store_id):
     user_id = session['userInfo']['userId']
     review = ReviewDAO.insert_review( user_id, store_id, request.form['contents'], request.form['rate'], request.form['image'])
-    return render_template('post/post.html', reviews=review)
+    store = StoreDAO().get_store_by_id(store_id)
+    images = ImageDAO().get_images_by_store_id(store_id)
+    menus = MenuDAO().get_menus_by_store_id(store_id)
+    return render_template('list/list_detail.html', store=store, images=images, menus=menus, reviews=review)
 
 @post_bp.route('/update/<int:store_id>/<int:review_id>', methods=['POST'])
 def update_review(store_id, review_id):
     review = ReviewDAO.update_review(request.form['contents'], request.form['rate'], review_id, store_id, request.form['image'])
-    return render_template('post/post.html', reviews=review)
+    store = StoreDAO().get_store_by_id(store_id)
+    images = ImageDAO().get_images_by_store_id(store_id)
+    menus = MenuDAO().get_menus_by_store_id(store_id)
+    return render_template('list/list_detail.html', store=store, images=images, menus=menus, reviews=review)
 
-@post_bp.route('/delete/<int:store_id>/<int:review_id>', methods=['GET'])
+@post_bp.route('/delete/<int:store_id>/<int:review_id>', methods=['POST'])
 def delete_review(store_id, review_id):
     review = ReviewDAO.delete_review(review_id, store_id)
-    return render_template('post/post.html', reviews=review)
+    store = StoreDAO().get_store_by_id(store_id)
+    images = ImageDAO().get_images_by_store_id(store_id)
+    menus = MenuDAO().get_menus_by_store_id(store_id)
+    return render_template('list/list_detail.html', store=store, images=images, menus=menus, reviews=review)
 
 
 # if __name__ == '__main__':
